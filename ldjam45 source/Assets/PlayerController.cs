da9086134aea16f87670d48891f362815a452d41
Carlos Costa <c.costa.gdev@gmail.com>
2019-10-05 13:07:10 +0100

    Switched to Unity xD

---
+++ ldjam45 source/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D body;
+    public Collider2D ground;
+    private int horizontalSpeed = 5;
+    private int jumpSpeed = 20;
+    private int gravity = -1;
+
+    private float yspeed = 0;
+    private float xspeed = 0;
+    private float xdir = 0;
+
+    public bool isOnGround = false;
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (body.IsTouching(ground))
+        {
+            isOnGround = true;
+        }
+        else isOnGround = false;
+
+        xdir = Input.GetAxisRaw("Horizontal");
+
+        if (isOnGround)
+        {
+            xspeed = xdir * horizontalSpeed;
+            yspeed = 0;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                yspeed += jumpSpeed;
+                isOnGround = false;
+            }
+        }
+        else
+        {
+            yspeed += gravity;
+            xspeed += xdir * 0.1f;
+        }
+
+        body.velocity = new Vector2(xspeed, yspeed);
+    }
+}
